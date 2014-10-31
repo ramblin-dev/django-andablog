08e203fb587366719c20d4120b6422bc6e8f97f3
Ivan VenOsdel <ivandv@gmail.com>
2014-10-31 13:40:48 -0500

    Added an entries sitemap

---
+++ djangoandablog/sitemaps.py
@@ -0,0 +1,23 @@
+from django.contrib.sitemaps import Sitemap
+
+from models import Entry
+
+
+class EntrySitemap(Sitemap):
+    """
+    Sitemap for entries.
+    """
+    priority = 0.5
+    changefreq = 'weekly'
+
+    def items(self):
+        """
+        Return published entries.
+        """
+        return Entry.objects.filter(is_published=True).order_by('-published_timestamp')
+
+    def lastmod(self, obj):
+        """
+        Return last modification of an entry.
+        """
+        return obj.modified
---
+++ djangoandablog/tests/test_sitemaps.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.test import TestCase
+from django.utils import timezone
+
+from djangoandablog import models, sitemaps
+
+
+class TestEntrySitemap(TestCase):
+
+    def setUp(self):
+        self.post_1 = models.Entry.objects.create(title=u'Welcome!', is_published=True)
+        self.post_2 = models.Entry.objects.create(title=u'Busy Busy', is_published=True)
+        self.post_3 = models.Entry.objects.create(title=u'Last Post', is_published=True)
+        self.post_4 = models.Entry.objects.create(title=u'Back again!')
+        self.entry_map = sitemaps.EntrySitemap()
+
+    def test_items(self):
+        """Only published entries should be listed by descending published timestamp"""
+        actual_entries = self.entry_map.items()
+
+        expected_slugs = ['last-post', 'busy-busy', 'welcome']
+        actual_slugs = [entry.slug for entry in actual_entries]
+
+        self.assertEquals(actual_slugs, expected_slugs)
+        self.assertNumQueries(1)
+
+    def test_last_modified(self):
+        """Should be able to get the last update from an entry"""
+        actual_update = self.entry_map.lastmod(self.post_3)
+
+        now = timezone.now()
+        self.assertGreaterEqual(now, actual_update)
+        self.assertGreaterEqual(actual_update, self.post_1.modified)
