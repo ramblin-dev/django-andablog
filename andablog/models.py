aa2645a4e8643e2da51c11e5fb6cf3e58f59feb7
Brad Montgomery <brad@bradmontgomery.net>
2015-10-17 13:05:50 -0500

    updating the slug truncate/de-duplicate logic so that it works with a maximum of 255 characters, and so that the appended timestamp won't include two dashes, eg: some-slug--12345689

--- andablog/models.py
+++ andablog/models.py
@@ -35,31 +35,31 @@ class Entry(TimeStampedModel):
     def get_absolute_url(self):
         return reverse('andablog:entrydetail', args=[self.slug])
 
-    def _insert_timestamp(self, value, max_length=50):
-        """Appends a timestamp integer to the given value, yet ensuring the
+    def _insert_timestamp(self, slug, max_length=255):
+        """Appends a timestamp integer to the given slug, yet ensuring the
         result is less than the specified max_length.
         """
         timestamp = str(int(time.time()))
-        value = "{}-{}".format(value, timestamp)
-        if len(value) > max_length:
-            value = '-'.join([value[:-len(timestamp)], timestamp])
-        return value
+        ts_len = len(timestamp) + 1
+        while len(slug) + ts_len > max_length:
+            slug = '-'.join(slug.split('-')[:-1])
+        slug = '-'.join([slug, timestamp])
+        return slug
 
     def _slugify_title(self):
-        """Slugify the Entry title, but ensure it's less than 50 characters.
-        This method also ensures that a slug is unique by appending a timestamp
-        to any duplicate slugs.
-
+        """Slugify the Entry title, but ensure it's less than the maximum
+        number of characters. This method also ensures that a slug is unique by
+        appending a timestamp to any duplicate slugs.
         """
-        # Restrict slugs to 50 chars, but don't split mid-word
+        # Restrict slugs to their maximum number of chars, but don't split mid-word
         self.slug = slugify(self.title)
-        while len(self.slug) > 50:
+        while len(self.slug) > 255:
             self.slug = '-'.join(self.slug.split('-')[:-1])
 
         # Is the same slug as another entry?
         if Entry.objects.filter(slug=self.slug).exclude(id=self.id).exists():
-            # Append time to differentiate, but keep total length < 50 chars.
-            self.slug = self._insert_timestamp(self.slug, max_length=50)
+            # Append time to differentiate.
+            self.slug = self._insert_timestamp(self.slug)
 
     def save(self, *args, **kwargs):
         self._slugify_title()
